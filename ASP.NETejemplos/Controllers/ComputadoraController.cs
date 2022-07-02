aea79e8a1bf2d445adffb83f483b86feb4aab525
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2022-07-01 21:21:17 -0400

    Routes

--- ASP.NETejemplos/Controllers/ComputadoraController.cs
+++ ASP.NETejemplos/Controllers/ComputadoraController.cs
@@ -1,5 +1,6 @@
 ﻿using ASP.NETejemplos.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace ASP.NETejemplos.Controllers
 {
@@ -15,6 +16,38 @@ namespace ASP.NETejemplos.Controllers
                 cantidad = 10
             };
             ViewBag.computadora = c;
+            return View();
+        }
+        public IActionResult Lista()
+        {
+            List<Computadora> computadoras = new List<Computadora>() {
+                 new Computadora()
+            {
+                id = "comp01",
+                nombre = "Laptop Toshiba",
+                imagen = "comp01.jpg",
+                precio = 1300.5,
+                cantidad = 10
+            },
+                  new Computadora()
+            {
+                id = "comp02",
+                nombre = "Laptop Compaq",
+                imagen = "comp02.jpg",
+                precio = 2000,
+                cantidad = 20
+            },
+                   new Computadora()
+            {
+                id = "comp03",
+                nombre = "Laptop HP",
+                imagen = "comp03.jpeg",
+                precio = 5050.5,
+                cantidad = 5
+            }
+            };
+            
+            ViewBag.computadora = computadoras;
             return View();
         }
     }
--- ASP.NETejemplos/Controllers/PersonaController.cs
+++ ASP.NETejemplos/Controllers/PersonaController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace ASP.NETejemplos.Controllers
 {
+    [Route("Ciudadano")]
     public class PersonaController : Controller
     {
+        [Route("indice")]
         public IActionResult Index()
         {
             ViewBag.edad = 18;
@@ -12,6 +16,21 @@ namespace ASP.NETejemplos.Controllers
             ViewBag.casado = false;
             ViewBag.estatura = 1.8;
             ViewBag.fecha_nac = DateTime.Now;
+            return View();
+        }
+        [Route("valores/{edad}")]
+        public IActionResult PruebasAppSettings(int edad)
+        {
+            var constructor = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var configuracion = constructor.Build();
+            ViewBag.valor1 = configuracion["ValorClave"];
+            ViewBag.valor2 = configuracion["MisAjustes:ajusteDev"];
+            ViewBag.valor3 = configuracion["MisAjustes:ajusteProd"];
+            ViewBag.valor4 = configuracion["MisAjustes:ajusteDebug"];
+            ViewBag.valor5 = configuracion["Logging:LogLevel:Microsoft"];
+            ViewBag.valor6 = edad;
             return View();
         }
     }
